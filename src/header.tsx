94a743775a5cc3e5e3f4dca036ce8d5a19f14e33
Aurora <aurora@aurorah.art>
2025-11-18 03:45:43 -0800

    ???
    This reverts commit 7d6242946512b92922692c50e4b331e247c827ca.

---
+++ src/header.tsx
@@ -0,0 +1,5 @@
+import { useState, useEffect } from 'preact/hooks'
+
+export function Header() {
+  useEffect(() => { }, []);
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { hydrate } from 'preact'
 import './index.css'
 import { Page } from './page.tsx'
+import { Header } from './header.tsx'
 
 hydrate(<Page />, document.getElementById('app')!)
